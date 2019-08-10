345954e5f894c4a03e5037123fb45a3af47067a4
charan karki <charankarki.31@gmail.com>
2019-08-10 10:32:08 +0530

    file added

---
+++ js/script.js
@@ -0,0 +1,65 @@
+
+$(document).ready(function () {
+    // navbar
+    $('#navbarId').on('hide.bs.collapse ',function(){$('#navbarId').slideUp()})
+    $('#navbarId').on('show.bs.collapse ',function(){$('#navbarId').slideDown()})
+
+
+    window.onscroll = function(){
+        
+
+        if($('.navbar').offset().top<=50){
+            $('.navbar').removeClass(' bg-light').addClass('navbar-dark')
+            $('#navbarId .nav-item .nav-link ,.navbar-brand').css('color','white')
+
+        }
+        else{
+            $('.navbar').addClass('navbar-light bg-light').removeClass('navbar-dark')
+            $('#navbarId .nav-item .nav-link,.navbar .navbar-brand').css('color','#06BCC1')
+        }
+        
+        
+    }
+    
+  
+
+
+        // scroll to top
+        $(".scroll-to-top").click(function(){
+            window.scroll({
+                top: 0, 
+                left: 0, 
+                behavior: 'smooth'
+              });
+            
+        })
+        // aimation 
+
+
+        // scroll animation
+      anime({
+          targets:'#header .jumbotron .btn',
+          translateX:[50,0],
+          opacity:[0,1],
+          easing: 'linear',
+
+      })
+
+    
+      var controller = new ScrollMagic.Controller()
+
+      var scene = new ScrollMagic.Scene({
+          triggerElement:   '#work',
+          duration:400,
+
+      }).setAnime(anime({
+          targets:'#work .row .col-sm-12',
+          translateY:[100,0],
+          opacity:[0,1],
+          delay:anime.stagger(200)
+      })).addTo(controller)
+      
+   
+
+
+});
